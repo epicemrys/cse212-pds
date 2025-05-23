f39502d578d585be27cbb74d1aa80f2a033613d3
epicemrys <emrysweb@outlook.comm>
2025-05-23 19:24:56 +0100

    Sets and Maps Problem Solved

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -10,6 +10,8 @@
  * 
  * Each row represents the player's stats for a single season with a single team.
  */
+         // basketball.csv file move into teach/bin to be able to run 
+
 
 using Microsoft.VisualBasic.FileIO;
 
@@ -17,20 +19,33 @@ public class Basketball
 {
     public static void Run()
     {
+        // Console.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
         var players = new Dictionary<string, int>();
 
         using var reader = new TextFieldParser("basketball.csv");
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+            if (players.ContainsKey(playerId))
+                players[playerId] += points;
+            else
+                players[playerId] = points;
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        // Console.WriteLine($"Players: {string.Join(", ", players)}");
 
-        var topPlayers = new string[10];
+        var topPlayers = players.ToArray();
+        Array.Sort(topPlayers, (p1, p2) => p2.Value - p1.Value);
+
+        Console.WriteLine();
+        for (var i = 0; i < 10; ++i)
+        {
+            Console.WriteLine(topPlayers[i]);
+        }
     }
 }
